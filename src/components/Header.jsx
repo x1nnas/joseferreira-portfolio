ab743580a05c03b423706998278330b375ecfeb3
Gonçalo Dias <goncalorbdias@gmail.com>
2026-03-27 12:23:04 +0000

    change header opacity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,17 @@ import Navigation from "./Navigation";
 // Header component for the application
 const Header = () => {
   return (
-    <nav className="fixed w-full z-20 backdrop-blur bg-white/10 shadow-lg border-b border-black/20">
+    <nav className="fixed w-full z-20 backdrop-blur bg-white/02 shadow-lg border-b border-black/20">
       {/* Container for the header content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16">
           {/* Logo linking to the homepage */}
-          <Link to="/" className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white tracking-normal whitespace-nowrap">
-            <span className="text-cyan-400">Jose</span> M.S. <span className="text-cyan-400">Ferreira</span>
+          <Link
+            to="/"
+            className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white tracking-normal whitespace-nowrap"
+          >
+            <span className="text-cyan-400">Jose</span> M.S.{" "}
+            <span className="text-cyan-400">Ferreira</span>
           </Link>
           {/* Navigation component */}
           <Navigation />
